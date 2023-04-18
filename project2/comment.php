8fcbb84753aad759e78572645e6a586fa8ea3f98
hari kiran <113755124+harikiran5184@users.noreply.github.com>
2023-04-18 15:10:22 +0530

    Delete project2 directory

--- project2/comment.php
+++
@@ -1,9 +0,0 @@
-<?php
-ob_start();
-session_start();
-$conn=mysqli_connect($_SESSION['l'],$_SESSION['r'],$_SESSION['p'],$_SESSION['d']);
-$conn->query("insert into comments values('$_POST[cfilename]','$_SESSION[name]','$_POST[comment]')");
-if($conn){
-echo $_POST['comment'];
-}
-?>
--- project2/fpr.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['vpf']=$_POST['reqto'];
-echo $_SESSION['vpf'];
-?>
